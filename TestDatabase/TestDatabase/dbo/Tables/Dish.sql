3d32090cf9551efd55d21b566449405b668d16a9
Filip Łada <filip.lada@kotrak.com>
2017-03-23 23:24:16 +0100

    ThirdBranch commit

--- TestDatabase/TestDatabase/dbo/Tables/Dish.sql
+++ TestDatabase/TestDatabase/dbo/Tables/Dish.sql
@@ -2,6 +2,7 @@
     [nazwa]                 NVARCHAR (50) NULL,
     [cena]                  INT           NULL,
     [mozliwa_l_zamowien]    INT           NULL,
-    [czyJestWegetarianskie] BIT           NULL
+    [czyJestWegetarianskie] BIT           NOT NULL, 
+    [czyZMiesem] BIT NOT NULL
 );
 
